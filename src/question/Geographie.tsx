4e817241bee4ad6895be76e346b0854453a44c04
Charles <charleszecevic@gmail.com>
2024-07-10 18:13:01 +0200

    add corgi / good / bad response

--- src/question/Geographie.tsx
+++ src/question/Geographie.tsx
@@ -3,6 +3,8 @@ import { useSpring, animated } from "@react-spring/web";
 import { data } from "../data/data";
 import { Link } from "react-router-dom";
 import corgiHappy from "../assets/happy-corgi-with-militaire-hat.png";
+import goodCorgi from "../assets/corgi.png";
+import sadCorgi from "../assets/sad-corgi.png";
 
 export const Geographie = () => {
   const [questionIndex, setQuestionIndex] = useState<number>(
@@ -10,13 +12,20 @@ export const Geographie = () => {
   );
   const [point, setPoint] = useState<number>(0);
   const [showVictory, setShowVictory] = useState<boolean>(false);
+  const [showResponse, setShowResponse] = useState<
+    "correct" | "incorrect" | null
+  >(null);
   const currentQuestion = data[questionIndex];
 
   const getRandomQuestion = (res: string) => {
     if (res === data[questionIndex].goodResponse) {
       setPoint(point + 1);
+      setShowResponse("correct");
+    } else {
+      setShowResponse("incorrect");
     }
     setQuestionIndex(Math.floor(Math.random() * data.length));
+    setTimeout(() => setShowResponse(null), 1000);
   };
 
   useEffect(() => {
@@ -24,6 +33,12 @@ export const Geographie = () => {
       setShowVictory(true);
     }
   }, [point]);
+
+  const feedbackAnimation = useSpring({
+    opacity: showResponse ? 1 : 0,
+    transform: showResponse ? "scale(1)" : "scale(0.5)",
+    config: { duration: 500 },
+  });
 
   const victoryAnimation = useSpring({
     opacity: showVictory ? 1 : 0,
@@ -33,7 +48,9 @@ export const Geographie = () => {
 
   return (
     <div className="flex flex-col items-center gap-20 justify-center">
-      <p className=" absolute top-10 right-10">Score {point} / 10</p>
+      <p className=" absolute top-10 right-10 text-4xl z-10">
+        Score {point} / 10
+      </p>
       <h1 className="text-4xl font-bold text-blue-500">Geographie</h1>
       <div className="flex flex-col items-center gap-5 justify-center">
         <h1 className="text-2xl font-bold text-blue-500">
@@ -54,10 +71,30 @@ export const Geographie = () => {
           </button>
         </div>
       </div>
+      {showResponse && (
+        <animated.div
+          style={feedbackAnimation}
+          className=" absolute bg-zinc-800 p-8 rounded-md flex flex-col items-center justify-center h-screen"
+        >
+          <h1 className=" text-2xl font-bold text-blue-500">
+            Bravo vous avez réussi le quiz
+          </h1>
+          <img
+            src={showResponse === "correct" ? goodCorgi : sadCorgi}
+            alt="winImage"
+            className=" w-2/4"
+          ></img>
+          {showResponse === "correct" ? (
+            <p>Bonne réponse</p>
+          ) : (
+            <p>Mauvaise reponse</p>
+          )}
+        </animated.div>
+      )}
       {showVictory && (
         <animated.div
           style={victoryAnimation}
-          className=" absolute bg-zinc-800 p-8 rounded-md flex flex-col items-center"
+          className=" absolute bg-zinc-800 p-8 rounded-md flex flex-col items-center justify-center"
         >
           <h1 className=" text-2xl font-bold text-blue-500">
             Bravo vous avez réussi le quiz
